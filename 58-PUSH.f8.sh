1ff5febc7d208a6a33a8d16e6205f56f13be142f
Martin Milan <martin12333@users.noreply.github.com>
2022-11-23 19:03:01 +0100

    bk

---
+++ 58-PUSH.f8.sh
@@ -0,0 +1,27 @@
+(mm_f8_doc file pseudo-format)
+# (selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+(do-not run this file as a whole)
+)
+exit
+
+echo '-------- section --------'
+
+C:\pf\Git\bin\bash.exe
+
+
+echo '-------- section --------'
+
+	git add -vv  'README*'
+
+echo '-------- section --------'
+
+	git add -vv  '*.f8.*'
+	git add -vv --dry-run '*.sh'
+	git add -vv  '*.sh'
+
+	git commit -v -a -m bk
+
+	git push
+
+	git show --name-only
+
