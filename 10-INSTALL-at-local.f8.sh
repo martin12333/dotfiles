d068a7b692ec6524587ad739758580fa83c0b6c4
Martin Milan <martin12333@users.noreply.github.com>
2022-11-23 19:17:23 +0100

    bk

---
+++ 10-INSTALL-at-local.f8.sh
@@ -0,0 +1,16 @@
+
+(mm_f8_doc file pseudo-format)
+# (selected lines from this file can be run in a terminal ... e.g. by the F8 key, customly bound to runSelectedText  )
+
+do-not run this file as a whole
+)
+exit
+
+echo '-------- section --------'
+
+
+
+MOVING TO dotfiles/*-*.f8.sh FROM  C:\Users\marti\OneDrive\linux-f8\30-linux-setup.f8.sh
+
+
+
